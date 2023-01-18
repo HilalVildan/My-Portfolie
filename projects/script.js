95005df9e86de49aa26e50f169d14a053127e3e0
HilalVildan <hilalsarioglu96@gmail.com>
2023-01-18 13:43:18 +0100

    update

--- projects/script.js
+++ projects/script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
 document.addEventListener("visibilitychange", function () {
   if (document.visibilityState === "visible") {
-    document.title = "Projects | Portfolio Jigar Sable";
+    document.title = "Projects | Portfolio Hilal Vildan Sarioglu";
     $("#favicon").attr("href", "/assets/images/favicon.png");
   } else {
     document.title = "Come Back To Portfolio";
